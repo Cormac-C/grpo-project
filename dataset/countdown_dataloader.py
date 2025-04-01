54f51b04477bfa89d15362c9e5c52a56423d7f4b
abhijeetpraveen <abhijeetpraveen@gmail.com>
2025-04-01 14:34:31 -0400

    added dataloader and reward functions

---
+++ dataset/countdown_dataloader.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+import json
+from typing import List, Dict
+
+class Countdown(Dataset):
+    def __init__(self, json_path: str):
+        """
+        PyTorch dataset for the Countdown problem.
+        Loads data from a JSON file. Each entry must have 'target' and 'numbers'.
+        """
+        with open(json_path, 'r') as f:
+            self.data: List[Dict] = json.load(f)
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        item = self.data[idx]
+
+        target = item['target']
+        numbers = item['numbers']
+        
+        sample = {
+            "target": target,
+            "numbers": numbers
+        }
+
+        return sample
