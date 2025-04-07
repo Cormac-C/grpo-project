83a0031b2b0ea9ab37f46f1fd6527101cc626923
Cormac Cureton <cormaccureton@gmail.com>
2025-04-06 22:23:45 -0400

    Function to calculate log probabilities

--- grpo.py
+++ grpo.py
@@ -14,13 +14,13 @@ logger = logging.getLogger(__name__)
 
 
 def grpo_iteration(
-    d_b, policy_model, reward_model, tokenizer, optimizer, G, eps, beta, mu
+    query_batch, policy_model, reward_model, tokenizer, optimizer, G, eps, beta, mu
 ):
     """
     Perform one iteration of the GRPO algorithm.
 
     Args:
-        d_b: Batch of queries.
+        query_batch: Batch of queries.
         policy_model: The current policy model.
         reward_model: The reward model.
         tokenizer: The tokenizer for the policy model.
@@ -33,11 +33,13 @@ def grpo_iteration(
     Returns:
         The updated policy.
     """
-    # Sample G outputs from the policy for each query in d_b
-    outputs = sample_outputs(policy_model, tokenizer, d_b, G)
+    # Sample G outputs from the policy for each query in query_batch
+    outputs = sample_outputs(policy_model, tokenizer, query_batch, G)
 
     # Compute rewards and accuracies for each output
-    rewards, accuracies = calculate_rewards_and_accuracies(d_b, outputs, reward_model)
+    rewards, accuracies = calculate_rewards_and_accuracies(
+        query_batch, outputs, reward_model
+    )
 
     # Compute token-level advantage for each token in each output
     advantages = calculate_grpo_advantage(rewards)
@@ -64,14 +66,14 @@ def grpo_iteration(
     return policy_model
 
 
-def sample_outputs(policy, tokenizer, d_b, G):
+def sample_outputs(policy, tokenizer, query_batch, G):
     """
-    Sample G outputs from the policy for each query in d_b. Doesn't track gradients or log probs.
+    Sample G outputs from the policy for each query in query_batch. Doesn't track gradients or log probs.
 
     Args:
         policy: The current policy.
         tokenizer: The tokenizer for the policy model.
-        d_b: Batch of queries, a list of strings of length batch_size.
+        query_batch: Batch of queries, a list of strings of length batch_size.
         G: The number of outputs to sample.
 
     Returns:
@@ -89,7 +91,7 @@ def sample_outputs(policy, tokenizer, d_b, G):
 
     # Tokenize the batch of queries
     tokenized_queries = tokenizer(
-        d_b, return_tensors="pt", padding=True, truncation=True
+        query_batch, return_tensors="pt", padding=True, truncation=True
     )
 
     # Move the tokenized batch to the device of the policy
@@ -98,7 +100,7 @@ def sample_outputs(policy, tokenizer, d_b, G):
     }
 
     # TODO: Revisit if it makes sense to no_grad here
-    # Sample G outputs from the policy for each query in d_b
+    # Sample G outputs from the policy for each query in query_batch
     with torch.no_grad():
         output = policy.generate(**tokenized_queries, generation_config=gen_config)
 
@@ -113,7 +115,7 @@ def sample_outputs(policy, tokenizer, d_b, G):
     batch_responses = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
     # logger.info(f"Batch responses: {batch_responses}")
     # Reshape the outputs to have shape (batch_size, G), each item contains the generated text and log probs
-    batch_size = len(d_b)
+    batch_size = len(query_batch)
     responses_reshaped = [
         batch_responses[i * G : (i + 1) * G] for i in range(batch_size)
     ]
@@ -128,12 +130,12 @@ def sample_outputs(policy, tokenizer, d_b, G):
     return generated_ids_reshaped, responses_reshaped
 
 
-def calculate_rewards_and_accuracies(d_b, outputs, reward_model):
+def calculate_rewards_and_accuracies(query_batch, outputs, reward_model):
     """
     Calculate the rewards for each output across the batch of queries.
 
     Args:
-        d_b: Batch of queries.
+        query_batch: Batch of queries.
         outputs: The sampled outputs.
         reward_model: The reward model.
     Returns:
@@ -141,11 +143,11 @@ def calculate_rewards_and_accuracies(d_b, outputs, reward_model):
     """
     # TODO: revisit if this is the best structure
     # Rewards are scalars so shape is (batch_size, G)
-    rewards = torch.zeros(len(d_b), outputs.shape[1])
-    accuracies = torch.zeros(len(d_b), outputs.shape[1])
+    rewards = torch.zeros(len(query_batch), outputs.shape[1])
+    accuracies = torch.zeros(len(query_batch), outputs.shape[1])
 
     # Assume reward model can accomodate a batch of queries and outputs
-    metrics = reward_model(d_b, outputs)
+    metrics = reward_model(query_batch, outputs)
     rewards = metrics["reward_score"]
     accuracies = metrics["accuracy"]
     return rewards, accuracies
@@ -161,9 +163,6 @@ def calculate_grpo_advantage(rewards):
     """
 
     advantages = rewards.clone()
-    # Calculate the advantage for each output
-    # The advantage is the difference between the reward and the mean reward for that output
-    # across the batch of queries
     for i in range(rewards.shape[0]):
         group_mean = torch.mean(rewards[i])
         group_std = torch.std(rewards[i])
@@ -172,6 +171,67 @@ def calculate_grpo_advantage(rewards):
 
     logger.info(f"Advantages shape: {advantages.shape}")
     return advantages
+
+
+def compute_log_probs(policy, tokenizer, query_batch, generated_ids):
+    """
+    Calculate log probabilities for the generated IDs for a given policy.
+    Args:
+        policy: The current policy.
+        tokenizer: The tokenizer for the policy model.
+        query_batch: Batch of queries, should be of shape (batch_size).
+        generated_ids: The generated IDs, should be of shape (batch_size, G, max_length).
+    Returns:
+        Log probabilities for the generated IDs, should be of shape (batch_size, G, max_length).
+
+    """
+    # TODO: Chill out the comments in this function
+    # Tokenize the batch of queries
+    tokenized_queries = tokenizer(
+        query_batch, return_tensors="pt", padding=True, truncation=True
+    )
+    query_ids = tokenized_queries["input_ids"]
+    # Expand the query IDs to match the shape of generated IDs
+    query_ids = query_ids.unsqueeze(1).expand(-1, generated_ids.shape[1], -1)
+    logger.info(f"Query IDs shape: {query_ids.shape}")
+    logger.info(f"Generated IDs shape: {generated_ids.shape}")
+
+    # Concatenate the query IDs and generated IDs
+    input_ids = torch.cat((query_ids, generated_ids), dim=2)
+    logger.info(f"Input IDs shape: {input_ids.shape}")
+    # Reshape the input IDs to have shape (batch_size * G, max_length)
+    input_ids = input_ids.view(-1, input_ids.shape[-1])
+    logger.info(f"Reshaped Input IDs shape: {input_ids.shape}")
+
+    # Move the input IDs to the device of the policy
+    input_ids = input_ids.to(policy.device)
+    # Get the attention mask
+    attention_mask = input_ids.ne(tokenizer.pad_token_id).long().to(policy.device)
+    logger.info(f"Attention mask shape: {attention_mask.shape}")
+
+    # Run forward pass to get the logits
+    outputs = policy(input_ids=input_ids, attention_mask=attention_mask)
+    logits = outputs.logits
+    logger.info(f"Logits shape: {logits.shape}")
+
+    # Separate the logits for the generated IDs
+    generated_logits = logits[:, query_ids.shape[2] :]
+    logger.info(f"Generated logits shape: {generated_logits.shape}")
+
+    # Calculate log probabilities
+    log_probs = F.log_softmax(generated_logits, dim=-1)
+    logger.info(f"Log probabilities shape: {log_probs.shape}")
+    # Get the log probabilities for the generated IDs
+    generated_ids = generated_ids.view(-1, generated_ids.shape[-1])
+    log_probs = log_probs.gather(2, generated_ids.unsqueeze(-1)).squeeze(-1)
+    logger.info(f"Gathered log probabilities shape: {log_probs.shape}")
+
+    # Reshape the log probabilities to have shape (batch_size, G, max_length)
+    batch_size = len(query_batch)
+    log_probs = log_probs.view(batch_size, -1, generated_ids.shape[-1])
+    logger.info(f"Reshaped log probabilities shape: {log_probs.shape}")
+
+    return log_probs
 
 
 def kl_div_estimator(model_log_probs, ref_model_log_probs):
