b35f7ef49ed68f651487fb019d373a068c1c8676
Aman Sidhu <asidhu_ca@yahoo.ca>
2025-04-18 02:06:12 -0400

    comment out tokenizer pad = tokenizer eos

--- scripts/train.py
+++ scripts/train.py
@@ -160,7 +160,7 @@ def main():
         torch_dtype=POLICY_MODEL_PRECISION,
     )
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.pad_token = tokenizer.eos_token
+    # tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "left"
     model.to(device)
     logger.info("Policy Model loaded successfully.")
